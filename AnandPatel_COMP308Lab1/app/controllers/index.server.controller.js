26211b05fb670c2c278fbdd8df765bf4603137bd
Anand Patel <apate607@my.centennialcollege.ca>
2018-02-07 14:09:06 -0500

    Added feedbackpage.ejs and its contents

---
+++ AnandPatel_COMP308Lab1/app/controllers/index.server.controller.js
@@ -0,0 +1,28 @@
+﻿//This code uses the CommonJS module pattern to define a function named render()
+//You can require this module and use this function
+//You'll need to use Express routing functionality to utilize the controller
+exports.showFeedbackPage = function (req, res) {
+    //read the username and password from body property of request object
+    var username = req.body.username;
+    var password = req.body.password;
+    //make a reference to the session object
+    var session = req.session;
+    //store username in session object
+    session.username = username;
+    console.log("In index function - User name = " + session.username);
+    //check if session object contains the username
+    if (session.username) {
+        //display feedback page
+        res.render('anand_feedback', {
+            title: 'Feedback Page',
+            username: username
+        });
+    }
+    else {
+
+        //redirect to the root
+        res.redirect('/');
+
+    }
+    console.log("GET request - User name = " + session.username);
+};
---
+++ AnandPatel_COMP308Lab1/app/routes/index.server.routes.js
@@ -0,0 +1,20 @@
+﻿//This uses CommonJS module pattern to export a single module function.
+//This function takes an express object as argument 
+//Then it requires the index controller and uses its render() method
+//as a middleware to GET requests made to the root path.
+
+module.exports = function (app) {
+    //load the controllers in memory
+    var index = require('../controllers/index.server.controller');
+    
+    //handle the routing of get and post request
+    app.get('/', res.render('index',
+        { title: 'Login Page' }));
+    //
+    app.post('/', function (req, res) {
+        //console.log("POST request - User name = " + req.body.username);
+        index.showFeedbackPage(req, res);
+        
+    });
+
+};
